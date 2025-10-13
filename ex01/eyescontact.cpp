7899b9bc265389765d29dff896f2ab7c55378fe9
Flash <flash19@Flashs-MacBook-Pro.local>
2025-10-13 15:25:22 +0200

    start of ex01

---
+++ ex01/eyescontact.cpp
@@ -0,0 +1,30 @@
+class PhoneBook 
+{
+private:
+    // Attributs privés (accessibles uniquement dans la classe)
+    int attributPrive;
+
+public:
+    // Constructeur
+    NomDeLaClasse() {
+        attributPrive = 0;
+    }
+    
+    // Méthodes publiques
+    void maMethode() {
+        // code
+    }
+}
+
+class Contact
+{
+
+}
+
+int main() 
+{
+    PhoneBook phonebook;
+    
+    phonebook.maMethode();
+    return 0;
+}
